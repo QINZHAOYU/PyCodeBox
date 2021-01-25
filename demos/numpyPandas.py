6304b3ea78ddd2224d1c7fb4f09a8241e452631c
qinzhaoyu <17764586230@163.com>
2021-01-25 17:31:02 +0800

    init job;ref #2

---
+++ demos/numpyPandas.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+# -------------------------------------------------------------
+'''
+@Copyright :  Copyright(C) 2021,QinZhaoYu. All rights reserved.  
+@License   :  LGPL-3.0  
+
+@File      :  numpyPandas.py  
+@Time      :  2021/01/25  
+@Author    :  Qin ZhaoYu   
+@Version   :  1.0  
+
+@Desc      :  To learn numpy and pandas api.  
+
+Change History:
+---------------------------------------------------------------
+--version1.0, 2021/01/25, Qin ZhaoYu
+Nothing.
+---------------------------------------------------------------
+'''
+# -------------------------------------------------------------
+
+import numpy as np 
+import pandas as pd 
+
+
+
+class NumpyDemo():
+    '''To learn numpy api.
+    '''
+    pass
+
+
+
+class PandasDemo():
+    '''To learn pandas api.
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    npDemo = NumpyDemo()
+
+
