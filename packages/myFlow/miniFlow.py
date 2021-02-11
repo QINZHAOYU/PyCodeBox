6ae21f0a1001b80cd849b0ecd89b161bb9ce4da1
rain Q <1719885464@qq.com>
2021-02-11 12:39:28 +0800

    update framework; add deepLearn demo

---
+++ packages/myFlow/miniFlow.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+# -------------------------------------------------------------
+'''
+@File      :  miniFlow.py
+@Time      :  2021/01/24
+@Author    :  Qin ZhaoYu
+@Version   :  1.0
+
+@Desc      :  To implement a mini deep learing uniform framework.
+
+'''
+# -------------------------------------------------------------
+
+# Passed, for kaikeba lesson out of date.
